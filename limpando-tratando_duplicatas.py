a0aa8ee6dcf7580a5e7fde053d4a522d0ef8e1a6
unknown <lucasalexanddre@hotmail.com>
2022-01-22 01:31:41 -0300

    Tratando dados duplicados

---
+++ limpando-tratando_duplicatas.py
@@ -0,0 +1,21 @@
+# Tratando duplicatas
+
+import pandas as pd 
+
+ride_sharing = pd.read_csv("C:/Users/lucasMartins/Documents/DataCamp/Cleaning Data/ride_sharing_new.csv")
+
+# Drop complete duplicates from ride_sharing
+ride_dup = ride_sharing.drop_duplicates()
+
+# Create statistics dictionary for aggregation function
+statistics = {'user_birth_year': 'min', 'duration': 'mean'}
+
+# Group by ride_id and compute new statistics
+ride_unique = ride_dup.groupby('ride_id').agg(statistics).reset_index()
+
+# Find duplicated values again
+duplicates = ride_unique.duplicated(subset = 'ride_id', keep = False)
+duplicated_rides = ride_unique[duplicates == True]
+
+# Assert duplicates are processed
+assert duplicated_rides.shape[0] == 0
